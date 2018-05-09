93a046a0902f2fc3725e61465934d57d584b9b68
Mohammed Elobadi <elobadi@gmail.com>
2018-05-10 00:56:00 +0200

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
   coffee = require('gulp-coffee'),
+  browserify = require('gulp-browserify'),
   concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsScripts = [
+var jsSources = [
   'components/scripts/pixgrid.js',
   'components/scripts/rclick.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
-]
+];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
-    .pipe(coffee({
-        bare: true
-      })
+    .pipe(coffee({ bare: true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-  gulp.src(jsScripts)
+  gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
