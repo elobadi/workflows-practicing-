923de42338148964f8dddb5addd794eb999bff74
Mohammed Elobadi <elobadi@gmail.com>
2018-05-19 14:59:08 +0200

    Minifyed our HTML Files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
   gulpif = require('gulp-if'),
   uglify = require('gulp-uglify'),
   babel = require('gulp-babel'),
+  minifyHTML = require('gulp-minify-html'),
   concat = require('gulp-concat');
 
 var env,
@@ -81,7 +82,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -93,7 +94,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+  .pipe(gulpif(env === 'production', minifyHTML()))
+  .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
   .pipe(connect.reload());
 });
 
