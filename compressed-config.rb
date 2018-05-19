4c1d1041861a6d997be2d647b19f40644b1f693d
Mohammed Elobadi <elobadi@gmail.com>
2018-05-19 13:05:12 +0200

    Added Environment Variables and created 2 config.rb Files

---
+++ compressed-config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+sass_dir = 'components/sass'
+images_dir = 'components/images'
+javascripts_dir = 'components/scripts'
+relative_assets = true
+line_comments = true
+output_style = :compressed
